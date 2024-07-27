ae66d001aee9dfe4d639fba2c84f7de1b5f36e6a
donatto22 <ottanod22@gmail.com>
2024-07-26 23:10:53 -0500

    cambiando chakra por material

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,72 @@
-import { useEffect, useState } from 'react'
-import { Box } from '@chakra-ui/react'
+import { useEffect, useRef, useCallback, useState } from 'react'
 
 import Actions from './components/Actions'
 
 import WebCam from 'react-webcam'
 import feather from 'feather-icons'
+import { Box, IconButton, Stack } from '@mui/material'
 
 const App = () => {
-    useState()
+    const [photo, setPhoto] = useState(null)
+
+    const webcam = useRef(null)
+
+    const capture = useCallback(() => {
+        const img = webcam.current.getScreenshot()
+        setPhoto(img)
+    }, [])
 
     useEffect(() => {
         feather.replace()
     }, [])
 
     const videoConstraints = {
-        width: 1200,
-        height: 700
+        width: 800,
+        height: 500
     }
 
     return (
-        <Box w='100dvw' h='100dvh'>
-            <Box display='flex' flexDir='row' justifyContent='space-between' height='100%'>
+        <Box width='100dvw' height='100dvh'>
+            <Stack direction='row' justifyContent='space-between' height='100%'>
                 <Actions>
-                    <i data-feather="camera"></i>
+                    <ul>
+                        <li>
+                            <IconButton sx={{ color: 'var(--white)',
+                                margin: 0,
+                                '&:hover': {
+                                    backgroundColor: 'var(--white)',
+                                    color: 'black',
+                                    transition: 'all .2s'
+                                }
+                            }}><i data-feather="camera"></i></IconButton>
+                        </li>
+                    </ul>
                 </Actions>
 
                 <Box alignContent='center' textAlign='center'>
-                    <WebCam style={{ borderRadius: '10px', boxShadow: '0 40px 100px -80px var(--white)' }} videoConstraints={videoConstraints}/>
+                    <WebCam ref={webcam} videoConstraints={videoConstraints}
+                    style={{ borderRadius: '10px', boxShadow: '0 40px 100px -80px var(--white)' }}/>
+                    {
+                        photo && <img src={ photo } alt="captura" />
+                    }
                 </Box>
 
                 <Actions>
-                    <i data-feather="maximize"></i>
+                    <ul>
+                        <li>
+                            <IconButton sx={{
+                                color: 'var(--white)',
+                                margin: 0,
+                                '&:hover': {
+                                    backgroundColor: 'var(--white)',
+                                    color: 'black',
+                                    transition: 'all .2s'
+                                }
+                            }}><i data-feather="maximize"></i></IconButton>
+                        </li>
+                    </ul>
                 </Actions>
-            </Box>
+            </Stack>
         </Box>
     )
 }
--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -1,12 +1,12 @@
-import { Box } from '@chakra-ui/react'
-
+import { Stack } from '@mui/material'
 import PropTypes from 'prop-types'
 
 const Actions = ({ children }) => {
     return (
-        <Box bgColor='rgba(0, 0, 0, .4)' zIndex='100' position='relative' alignContent='center' color='var(--white)' w='70px' textAlign='center'>
-            { children }
-        </Box>
+        <Stack backgroundColor='rgba(0, 0, 0, 0.4)' position='relative' direction='column'
+            justifyContent='center' color='var(--white)' width='70px' alignItems='center'>
+                { children }
+        </Stack>
     )
 }
 
